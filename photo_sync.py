22ccdba38ce3385d33a1f9ac62f709cfee04a9b3
Gavin Hills <ghills@gmail.com>
2015-08-29 11:49:28 -0700

    Make it better handle existing file with matching name

--- photo_sync.py
+++ photo_sync.py
@@ -4,10 +4,12 @@ import os
 import exifread
 import shutil
 import logging
+import filecmp
+import uuid
 
 from datetime import datetime
 
-pic_extensions = ('.jpg', '.png', '.gif', '.bmp', '')
+pic_extensions = ('.jpg', '.png', '.gif', '.bmp', '.jpeg', '')
 video_extensions = ('.mov', '.mp4', '.avi')
 
 input_file_path = os.path.normpath("e:\\dropbox\\ifttt\\gmail")
@@ -30,19 +32,42 @@ def get_picture_date(fpath):
             logging.warning('No timestamp found for {}. tags: {}'.format(fpath, " ".join(tags.keys())))
     return timestamp.strftime('%Y-%m-%d')
 
+def mangle_filename(filename):
+	base, ext = os.path.splitext(filename)
+	return( str(uuid.uuid4()) + ext )
+	
+def create_destination_folder(path):
+	try:
+		logging.info('os.makedirs({})'.format(path))
+		if not dry_run:
+			os.makedirs(path)
+	except:
+		logging.exception('Error creating output directory {}'.format(path))
+			
 def move_file(fpath, outdir):
     if not os.path.exists(outdir):
-        try:
-            logging.info('os.makedirs({})'.format(outdir))
-            if not dry_run:
-                os.makedirs(outdir)
-        except:
-            logging.exception('Error creating output directory {}'.format(outdir))
+        create_destination_folder(outdir)
 
     try:
-        logging.info('shutil.move({}, {})'.format(fpath, outdir))
-        if not dry_run:
-            shutil.move(fpath, outdir)
+		filename = os.path.split(fpath)[1]
+		if os.path.exists(os.path.join(outdir, filename)):
+			logging.info('{} already exists'.format(os.path.join(outdir, filename)))
+			# check if the file is the same first
+			if filecmp.cmp(fpath, os.path.join(outdir, filename)):
+				logging.info('existing file equivalent.')
+				logging.info('os.remove({})'.format(fpath))
+				if not dry_run:
+					os.remove(fpath)
+			else:
+				# same filename, different contents. Rename it
+				filename = mangle_filename(filename)
+				logging.info('shutil.move({}, {})'.format(fpath, os.path.join(outdir, filename)))
+				if not dry_run:
+					shutil.move(fpath, os.path.join(outdir, filename))
+		else:
+			logging.info('shutil.move({}, {})'.format(fpath, outdir))
+			if not dry_run:
+				shutil.move(fpath, outdir)
     except:
         logging.exception('Error moving {} to {}'.format(fpath, outdir))
 
